f27851fab94dce600529995d76af4988efcbca92
Sid <siddharth2704@gmail.com>
2019-01-14 16:13:47 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edited <code>src/App.js</code> and saved to reload. Try 1!
           </p>
           <a
             className="App-link"
